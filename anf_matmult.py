8c7c3cc417f4fbb8ddb024fe869a136f232ed0d8
yashpote <yashppote@gmail.com>
2021-02-25 03:51:47 +0800

    jupdage

--- anf_matmult.py
+++ anf_matmult.py
@@ -9,7 +9,7 @@ def check_mult(var_manager, inputA, inputB, output, n):
             string = ""
             for k in range(n):
                 string += row[k] + "*" + column[k] + " + "   
-            string += "+ " + output_element + " + 1"
+            string +=  output_element + " + 1"
             print(string)
     return 0
 
@@ -18,11 +18,24 @@ def node_variable_initializer(variable_manager, node_type, number, offset):
     return variable_manager, offset+number
 
 def sigma_connect(variable_manager, in_layer, out_layer,  offset):
-    
-    number = 0
-    return variable_manager, offset+number
+    for node_out in variable_manager[out_layer]:
+        connection = node_out + " + "
+        for node_in in variable_manager[in_layer]:
+            connection += node_in + "*x" + str(offset) + " + "
+            offset += 1
+        connection += "1"
+        print(connection)
+ 
+    return variable_manager, offset
 
-def pi_connect(variable_manager, in_layer, out_layer,  offset):
+def pi_connect(variable_manager, in_layer, out_layer, fanin, offset):
+
+    in_layer = variable_manager[in_layer[0]] +  variable_manager[in_layer[1]]
+
+    #for node_out in variable_manager[out_layer]:
+    #    connection = node_out + " + "
+    #    for node_in in variable_manager[in_layer]:
+    #        connection += node_in + 
 
     number = 0
     return variable_manager, offset+number
@@ -32,10 +45,11 @@ def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--size", type=int, help='input side length of the matrix')
     parser.add_argument("--pi", type=int, help='gates in pi layer')
-
+    parser.add_argument("--fanin", type=int, help='fan in for and gates')
     args = parser.parse_args()
     n = args.size
-    
+    fanin = args.fanin
+
     num_pi_nodes = 0 
 
     if args.pi is None:
@@ -55,7 +69,7 @@ def main():
 
     var_manager, offset = sigma_connect(var_manager, 'matA', 'input_sigmaA', offset)
     var_manager, offset = sigma_connect(var_manager, 'matB', 'input_sigmaB', offset)
-    var_manager, offset = pi_connect(var_manager, ['input_sigmaA', 'input_sigmaB'], 'middle_pi', offset)
+    var_manager, offset = pi_connect(var_manager, ['input_sigmaA', 'input_sigmaB'], 'middle_pi',fanin , offset)
     var_manager, offset = sigma_connect(var_manager, 'middle_pi', 'output_sigma', offset)
 
     check_mult(var_manager, 'matA', 'matB', 'output_sigma', n)
